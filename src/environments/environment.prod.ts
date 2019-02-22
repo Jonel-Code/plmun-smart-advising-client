5e339e2ad15e4d97335b44c5f72d7a6d91d517a5
Jonel-Code <akoilypants3@gmail.com>
2019-02-22 13:31:04 +0800

    added Student data Route on Prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
         {title: 'Advising Statistics', link: 'advising_statistics'},
         {title: 'Opened Subjects', link: 'opened_subjects'},
         {title: 'Curriculum', link: 'admin_curriculum'},
+        {title: 'Student Data', link: 'student_data'},
         {title: 'Faculty Accounts', link: 'faculty_accounts'}
     ],
 };
