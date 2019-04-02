f832e07b1d03a5fbc4919358d44dcfbc0cc1b1ce
Jonel-Code <akoilypants3@gmail.com>
2019-04-02 18:24:51 +0800

    added max days on express middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SMARTCClient'));
+app.use(express.static(__dirname + '/dist/SMARTCClient', { maxAge: '30 days' }));
 
 app.get('/*', function(req,res) {    
     res.sendFile(path.join(__dirname+'/dist/SMARTCClient/index.html'));
